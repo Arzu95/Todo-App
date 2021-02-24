4f0e19d7294c89c8e9094744a07a3fe29d9b63ed
Arzu95 <muhammadarzu3432@gmail.com>
2021-02-24 13:58:19 +0700

    Hari ke Delapan

---
+++ Todo App/Theme/ThemeModel.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeModel.swift
+//  Todo App
+//
+//  Created by Apple on 24/02/21.
+//
+
+import SwiftUI
+
+struct Theme: Identifiable {
+    let id: Int
+    let themeName: String
+    let themeColor: Color
+}
